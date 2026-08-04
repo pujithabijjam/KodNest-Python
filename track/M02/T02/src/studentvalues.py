27f8a8aa8eb3290f936900adbcbe1b3f712a211a
Bijjam Pujitha <pujithabijjam@gmail.com>
2026-08-04 17:26:12 +0530

    Studentvalues

---
+++ track/M02/T02/src/studentvalues.py
@@ -0,0 +1,12 @@
+age_text = "21"
+course_fee_text = "499.50"
+attempts = 3
+age = int(age_text)
+course = float(course_fee_text)
+attempts = str(attempts)
+print(age)
+print(type(age))
+print(course)
+print(type(course))
+print(attempts)
+print(type(attempts))
