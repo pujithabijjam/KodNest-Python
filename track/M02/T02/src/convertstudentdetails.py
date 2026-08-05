a495384ed9861851edffed1c1a44c1c294b77029
Bijjam Pujitha <pujithabijjam@gmail.com>
2026-08-05 08:50:00 +0530

    convertstudentdetails

---
+++ track/M02/T02/src/convertstudentdetails.py
@@ -0,0 +1,9 @@
+Student_name = "Meena"
+print(f"Student: {Student_name}")
+age = 21
+print(f"Age: {age}")
+rating = 4.8
+print(f"Rating: {rating}")
+print(type(Student_name))
+print(type(age))
+print(type(rating))
