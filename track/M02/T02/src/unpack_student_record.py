62534a21965c86c28fbb6a77de70a5a5ec4ebe8a
Bijjam Pujitha <pujithabijjam@gmail.com>
2026-08-07 15:41:59 +0530

    changes

---
+++ track/M02/T02/src/unpack_student_record.py
@@ -0,0 +1,8 @@
+name = input()
+course = input()
+score = input()
+student_record = (name,course,score)
+student_record = name,course,score
+print(f"Name: {name}")
+print(f"Course: {course}")
+print(f"Score: {score}")
