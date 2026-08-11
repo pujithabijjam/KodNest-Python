5fd4e70ff5027eb787d13a5ef0f4bd2c63fd3daa
Bijjam Pujitha <pujithabijjam@gmail.com>
2026-08-11 17:09:51 +0530

    completed data using slicing

---
+++ track/M02/T03/data_using_slicing.py
@@ -0,0 +1,9 @@
+word = input()
+first = int(input())
+second = int(input())
+third = int(input())
+numbers = [first, second, third]
+record = (first, second, third)
+print(f"Middle: {word[1:-1]}")
+print(f"First Two: {numbers[:2]}")
+print(f"Reversed Tuple: {record[::-1]}")
