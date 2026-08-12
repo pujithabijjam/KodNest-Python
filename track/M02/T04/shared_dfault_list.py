2926f09653422243b2302eb25c729548747dd655
Bijjam Pujitha <pujithabijjam@gmail.com>
2026-08-12 10:52:07 +0530

    completedshared default list

---
+++ track/M02/T04/shared_dfault_list.py
@@ -0,0 +1,9 @@
+def add_student(name, students = []):
+    students.append(name)
+    print(students)
+first_name = int(input())
+second_name = int(input())
+third_name = int(input())
+add_student(first_name)
+add_student(second_name)
+add_student(third_name)
